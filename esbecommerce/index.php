1a13ac8a4bf2a58440ab58853b2cc24078310faf
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-03-24 15:46:01 -0300

    adad

--- esbecommerce/index.php
+++ esbecommerce/index.php
@@ -14,7 +14,20 @@ include_once ('conexao');
 <body>
 <?php 
 
-echo "Kami House";
+echo "<h1>Kami House</h1> <br>";
+$query_disponiveis = "SELECT id, nome, descrição, preço FROM disponiveis ORDER BY id ASC";
+$resultado_disponiveis = $conn -> prepare($query_disponiveis);
+$resultado_disponiveis-> execute();
+
+while ($linha_disponiveis = $resultado_disponiveis-> fetch(PDO::FETCH_ASSOC)) {
+
+    echo "ID: ". $linha_disponiveis['id'].  "<br>";
+    echo "NOME: ". $linha_disponiveis['nome'].  "<br>";
+    echo "DESCRIÇÃO: ". $linha_disponiveis['descrição'].  "<br>";
+    echo "PREÇO: ". $linha_disponiveis['preço'].  "<br>";
+    echo "<hr>";
+
+};
 
 ?>
 </body>
