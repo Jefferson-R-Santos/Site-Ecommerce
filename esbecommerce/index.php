fc0edd5107b3488bbce45b659d952cddd071a6db
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-03-24 17:29:09 -0300

    sasdadc

--- esbecommerce/index.php
+++ esbecommerce/index.php
@@ -15,9 +15,21 @@ include_once ('conexao');
 <?php 
 
 echo "<h1>Kami House</h1> <br>";
-$query_disponiveis = "SELECT id, nome, descrição, preço FROM disponiveis ORDER BY id ASC";
+$query_disponiveis = "SELECT id, nome, descrição, preço, imagem FROM disponiveis ORDER BY id ASC";
 $resultado_disponiveis = $conn -> prepare($query_disponiveis);
 $resultado_disponiveis-> execute();
+
+while($row_disponiveis = $resultado_disponiveis-> fetch(PDO::FETCH_ASSOC)){
+
+    extract($row_disponiveis);
+echo "<img src= './imagens/$id/$imagem'><br>";
+echo "ID: $id <br>";
+echo "Nome: $nome <br>";
+echo "Preço: R$". number_format($preço, 2, ",", "."). "<br>";
+echo "<hr>";
+echo "";
+
+}
 
 ?>
 </body>
