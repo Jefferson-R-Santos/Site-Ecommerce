432f5e20c2d62396dea65eb8a42d9d3b2ab75d63
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-03-24 15:22:19 -0300

    inicio

---
+++ esbecommerce/index.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once ('conexao');
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Esborço de site Ecommerce</title>
+</head>
+<body>
+<?php 
+
+echo "Kami House";
+
+?>
+</body>
+</html>
