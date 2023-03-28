218e933c21d09a4a000b734e1419bb8c83994f23
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-03-28 15:26:36 -0300

    Update rpagamento.php

--- esbecommerce/rpagamento.php
+++ esbecommerce/rpagamento.php
@@ -1,4 +1,23 @@
 <?php
+
+$picpaytoken = "";
+
+//Dados da compra
+$dados = [
+    "referenceId" => $referenceId,
+    "callbackUrl"=> $callbackUrl,
+    "returnUrl"=> $returnUrl . $referenceId,
+    "value"=> $valor,
+    "expiresAt"=> $expiresAt,
+    "buyer"=> [ 
+      "firstName"=> $nome,
+      "lastName"=> $sobrenome,
+      "document"=> $documentos,
+      "email"=> $email,
+      "phone"=> $celular
+     ]
+];
+
 //iniciar Curl
 $pd = curl_init();
 
@@ -10,6 +29,23 @@ curl_setopt($pd, CURLOPT_RETURNTRANSFER, true);
 
 //Enviar o parametro referente ao SSL
 curl_setopt($pd, CURLOPT_SSL_VERIFYPEER, false);
+
+// Enviar dados da compra
+curl_setopt($pd, CURLOPT_POSTFIELDS, json_encode($dados));
+
+// Enviar headers
+$headers = [];
+$headers [] = 'Content_Type: application/json';
+$headers [] = 'x-picpay-token:'. $picpaytoken;
+curl_setopt($pd, CURLOPT_HTTPHEADER, $headers);
+
+//Realizar Requisição
+$resultado = curl_exec($pd);
+
+//Ler o Conteudo da resposta
+$dados_resultado = json_decode($resultado);
+
+//Imprimir o conteudo da resposta
 
 
 ?>
