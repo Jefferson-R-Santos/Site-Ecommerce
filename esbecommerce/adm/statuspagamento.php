2298575a75d4b4ce75eeed1f01867f496aaf773e
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-01 11:27:42 -0300

    alternando requisição de SSL para true

--- esbecommerce/adm/statuspagamento.php
+++ esbecommerce/adm/statuspagamento.php
@@ -1,11 +1,30 @@
 <?php 
 
+include_once '../token.php';
+
 $id_referencia = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
 if (!empty($id_referencia)) {
     var_dump($id_referencia);
+
+    //iniciar Curl
+$pd = curl_init();
+
+// URL de requisição picpay
+curl_setopt($pd, CURLOPT_URL, "https://appws.picpay.com/ecommerce/public/payments/$id_referencia/status");
+
+//Parametro de resposta da transferencia 
+curl_setopt($pd, CURLOPT_RETURNTRANSFER, true);
+
+//Enviar o parametro referente ao SSL
+curl_setopt($pd, CURLOPT_SSL_VERIFYPEER, true);
+
+
+
 } else {
     echo "Erro: Necessario enviar o Id de referencia <br>";
 }
+
+
 
 ?>
--- esbecommerce/rpagamento.php
+++ esbecommerce/rpagamento.php
@@ -40,7 +40,7 @@ curl_setopt($pd, CURLOPT_URL, 'https://appws.picpay.com/ecommerce/public/payment
 curl_setopt($pd, CURLOPT_RETURNTRANSFER, true);
 
 //Enviar o parametro referente ao SSL
-curl_setopt($pd, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($pd, CURLOPT_SSL_VERIFYPEER, true);
 
 // Enviar dados da compra
 curl_setopt($pd, CURLOPT_POSTFIELDS, json_encode($dados));
