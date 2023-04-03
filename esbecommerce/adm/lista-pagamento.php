25db05e277146d51ea7b3099c6ca06d0888b43c6
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-03 16:47:57 -0300

    Create lista-pagamento.php

---
+++ esbecommerce/adm/lista-pagamento.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+<meta charset="UTF-8">
+
+<title>Status de Pagamentos</title>
+
+</head>
+
+<body>
+
+<?php 
+
+echo "Lista de pagamentos";
+
+?>
+
+</body>
+
+</html>
