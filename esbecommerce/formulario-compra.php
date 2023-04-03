6924940417cb590de8f94f9f09766f3854a075c6
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-03 11:38:58 -0300

    incluindo token

--- esbecommerce/formulario-compra.php
+++ esbecommerce/formulario-compra.php
@@ -106,7 +106,7 @@ curl_setopt($pd, CURLOPT_URL, 'https://appws.picpay.com/ecommerce/public/payment
 curl_setopt($pd, CURLOPT_RETURNTRANSFER, true);
 
 //Enviar o parametro referente ao SSL
-curl_setopt($pd, CURLOPT_SSL_VERIFYPEER, true);
+curl_setopt($pd, CURLOPT_SSL_VERIFYPEER, false);
 
 // Enviar dados da compra
 curl_setopt($pd, CURLOPT_POSTFIELDS, json_encode($dados));
--- esbecommerce/token.php
+++ esbecommerce/token.php
@@ -1,6 +1,6 @@
 <?php 
 
-define('PICPAYTOKEN', "Token do Pic Pay");
+define('PICPAYTOKEN', "070e3567-ef60-440d-bef6-826a327e000e");
 define('CALLBACKURL', "http://www.sualoja.com.br/callback");
 define('RETURNURL', "http://www.sualoja.com.br/cliente/pedido/");
 
