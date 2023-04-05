19a0e269ad3d35f36af00dadff82791d7998236c
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-05 15:26:57 -0300

    Update funcao.php

--- esbecommerce/adm/funcao.php
+++ esbecommerce/adm/funcao.php
@@ -2,7 +2,35 @@
 
 function statusPagamento($id_referencia) {
 
-echo "id de referencia: $id_referencia <br>";
+//iniciar Curl
+$pd = curl_init();
+
+// URL de requisição picpay
+curl_setopt($pd, CURLOPT_URL, "https://appws.picpay.com/ecommerce/public/payments/$id_referencia/status");
+
+//Parametro de resposta da transferencia 
+curl_setopt($pd, CURLOPT_RETURNTRANSFER, true);
+
+//Enviar o parametro referente ao SSL
+curl_setopt($pd, CURLOPT_SSL_VERIFYPEER, false);
+
+// Enviar headers
+$headers = [];
+$headers [] = 'Content-Type: application/json';
+$headers [] = 'x-picpay-token:'. PICPAYTOKEN;
+curl_setopt($pd, CURLOPT_HTTPHEADER, $headers);
+
+//Realizar Requisição
+$resultado = curl_exec($pd);
+
+//Fechar conexão do curl
+curl_close($pd);
+
+//Ler o Conteudo da resposta
+$dados_resultado = json_decode($resultado);
+
+//Imprimir o conteudo da resposta
+var_dump($dados_resultado);
 
 }
 
