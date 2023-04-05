e26614972cbd4b54310adc2e92b4efb8e10e9c8c
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-05 15:10:05 -0300

    Update cancelar-pagamento.php

--- esbecommerce/adm/cancelar-pagamento.php
+++ esbecommerce/adm/cancelar-pagamento.php
@@ -1,5 +1,18 @@
 <?php 
+session_start();
+ob_start();
 
-echo "Cancelar Pagamento <br>";
+$id_referencia = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
+
+if (!empty($id_referencia)) {
+echo "Id de referencia: $id_referencia <br>";
+} else {
+    //Mensagem de Erro
+    $_SESSION ['msg'] = "<div class='alert alert-danger' role='alert'>
+    Erro: Selecione um Pagamento!
+   </div>";
+   //Redirecionar usuario
+      header("Location: lista-pagamentos.php ");
+}
 
 ?>
