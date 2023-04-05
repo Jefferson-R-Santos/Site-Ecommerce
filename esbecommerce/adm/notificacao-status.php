044da4edf23fc8333c0543901b107bb212332cfd
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-05 11:03:21 -0300

    Update notificacao-status.php

--- esbecommerce/adm/notificacao-status.php
+++ esbecommerce/adm/notificacao-status.php
@@ -7,4 +7,15 @@ $resultado_cabecalho = getallheaders();
 
 var_dump($resultado_cabecalho);
 var_dump($resultado_cabecalho['x-seller-token']);
+
+if ((isset($resultado_cabecalho['x-seller-token'])) AND !empty($resultado_cabecalho['x-seller-token'])) {
+    if ($resultado_cabecalho['x-seller-token'] == "95b8be0a-c56c-4961-906a-99694cd775f4") {
+        echo "Token Recebido é Válido <br>";
+    } else {
+        echo "Erro: Token Recebido é Inválido <br>";
+    }
+} else {
+    echo "Erro: Token Não Recebido <br>";
+}
+
 ?>
