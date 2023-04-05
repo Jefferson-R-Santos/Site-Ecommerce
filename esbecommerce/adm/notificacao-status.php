1fe8a0a6689e836e44972b9f7893eb250708fe1b
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-05 11:12:42 -0300

    configurando seller token

--- esbecommerce/adm/notificacao-status.php
+++ esbecommerce/adm/notificacao-status.php
@@ -3,13 +3,15 @@
 include_once '../token.php';
 include_once '../conexao.php';
 
+//Receber Cabeçalho 
 $resultado_cabecalho = getallheaders();
 
 var_dump($resultado_cabecalho);
-var_dump($resultado_cabecalho['x-seller-token']);
+
+//Validar Cabeçalho 
 
 if ((isset($resultado_cabecalho['x-seller-token'])) AND !empty($resultado_cabecalho['x-seller-token'])) {
-    if ($resultado_cabecalho['x-seller-token'] == "95b8be0a-c56c-4961-906a-99694cd775f4") {
+    if ($resultado_cabecalho['x-seller-token'] == XSELLERTOKEN) {
         echo "Token Recebido é Válido <br>";
     } else {
         echo "Erro: Token Recebido é Inválido <br>";
