f39d1e20f9f2c9e4432fdcb6e9844317fcc335c5
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-05 15:45:45 -0300

    Update cancelar-pagamento.php

--- esbecommerce/adm/cancelar-pagamento.php
+++ esbecommerce/adm/cancelar-pagamento.php
@@ -10,7 +10,28 @@ if (!empty($id_referencia)) {
 include_once './funcao.php';
 $dados_resultado = statusPagamento($id_referencia);
 
-var_dump($dados_resultado);
+ //iniciar Curl
+ $pd = curl_init();
+
+ // URL de requisição picpay
+curl_setopt($pd, CURLOPT_URL, "https://appws.picpay.com/ecommerce/public/payments/$id_referencia/cancellations");
+
+//Parametro de resposta da transferencia 
+curl_setopt($pd, CURLOPT_RETURNTRANSFER, true);
+
+//Enviar o parametro referente ao SSL
+curl_setopt($pd, CURLOPT_SSL_VERIFYPEER, false);
+
+//Informa que requisisão ser através do metodo POST
+curl_setopt($pd, CURLOPT_POST, true);
+
+if (isset($dados_resultado->authorizationId)) {
+
+    echo "Compra paga <br>";
+
+} else {
+    echo "Compra não paga <br>";
+}
 
 } else {
     //Mensagem de Erro
