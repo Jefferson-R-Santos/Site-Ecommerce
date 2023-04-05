1e28a7bf272ab50f3fe53c84ed09a27ada1c6862
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-05 11:36:01 -0300

    Update notificacao-status.php

--- esbecommerce/adm/notificacao-status.php
+++ esbecommerce/adm/notificacao-status.php
@@ -6,7 +6,7 @@ include_once '../conexao.php';
 //Receber Cabeçalho 
 $resultado_cabecalho = getallheaders();
 
-var_dump($resultado_cabecalho);
+//var_dump($resultado_cabecalho);
 
 //Validar Cabeçalho 
 
@@ -15,7 +15,14 @@ if ((isset($resultado_cabecalho['x-seller-token'])) AND !empty($resultado_cabeca
 
     //Obter Dados da Requisição
     $conteudo_req = json_decode( file_get_contents("php://input"));
-    var_dump($conteudo_req);
+    var_dump($conteudo_req->referenceId);
+    //iniciar Curl
+$pd = curl_init();
+
+// URL de requisição picpay
+//https://appws.picpay.com/ecommerce/public/payments/{referenceId}/status 
+curl_setopt($pd, CURLOPT_URL, "https://appws.picpay.com/ecommerce/public/payments/".$conteudo_req->referenceId."/status");
+
   
     } else {
         echo "Erro: Token Recebido é Inválido <br>";
