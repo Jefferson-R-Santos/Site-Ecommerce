056a59a3991fa110b1851d06d185e319cecf0009
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-06 17:06:05 -0300

    Update token.php

--- esbecommerce/token.php
+++ esbecommerce/token.php
@@ -1,7 +1,7 @@
 <?php 
 
-define('PICPAYTOKEN', "seu token");
-define('XSELLERTOKEN', "Seu seller token");
+define('PICPAYTOKEN', "070e3567-ef60-440d-bef6-826a327e000e");
+define('XSELLERTOKEN', "95b8be0a-c56c-4961-906a-99694cd775f4");
 define('CALLBACKURL', "http://www.sualoja.com.br/callback");
 define('RETURNURL', "http://www.sualoja.com.br/cliente/pedido/");
 
