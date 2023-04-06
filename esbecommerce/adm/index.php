dbba9b25ddadd1b7945357d3fbc886fa8057bfef
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-06 10:52:36 -0300

    Update index.php

--- esbecommerce/adm/index.php
+++ esbecommerce/adm/index.php
@@ -16,25 +16,25 @@
 <body>
     
 <form class = "form-signin">
-    <img class="mb-4" src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
-    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+    <img class="mb-4" src="../imagens/logo/Shopping free vector icons designed by Roundicons.png" alt="" width="72" height="57">
+    <h1 class="h3 mb-3 fw-normal">Faça Login abaixo para prosseguir.</h1>
 
     <div class="form-floating">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
+      <input type="email" class="form-control" id="floatingInput" placeholder="insira seu Email fulano@example.com">
+      <label for="floatingInput">Email </label>
     </div>
     <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Insira sua Senha">
+      <label for="floatingPassword">Senha</label>
     </div>
 
     <div class="checkbox mb-3">
       <label>
-        <input type="checkbox" value="remember-me"> Remember me
+        <input type="checkbox" value="remember-me"> Manter-se Conectado.
       </label>
     </div>
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
-    <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2023</p>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
+    <p class="mt-5 mb-3 text-body-secondary">&copy; 2023</p>
   </form>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
