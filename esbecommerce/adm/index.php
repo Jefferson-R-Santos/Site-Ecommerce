8daa50e6858fca68787fbd6048e22060b69288e8
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-06 11:24:40 -0300

    Update index.php

--- esbecommerce/adm/index.php
+++ esbecommerce/adm/index.php
@@ -95,16 +95,16 @@
     <h1 class="h3 mb-3 fw-normal">Admnistrador.</h1>
 
     <div class="form-floating">
-    <input type="email" class="form-control" id="email" name="email" placeholder="nome@example.com">
+    <input type="email" class="form-control" name="email" id="email" placeholder="E-mail">
       
     </div>
     <div class="form-floating">
-    <input type="password" class="form-control" id="senha" name="senha" placeholder="Insira sua Senha">
+    <input type="password" class="form-control" name="senha" id="senha" placeholder="Insira sua Senha">
       
     </div>
 
-    <button class="w-100 btn btn-lg btn-primary" type="submit">Entrar</button>
-    <p class="mt-5 mb-3 text-body-secondary">&copy;2023</p>
+    <button class="w-100 btn btn-lg btn-success" type="submit">Entrar</button>
+
   </form>
 </main>
 
