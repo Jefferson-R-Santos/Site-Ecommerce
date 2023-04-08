16a57b1b18d1e229b0b6f7f9dcae4c8e2c22ff4b
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-08 14:20:55 -0300

    Update view-produto.php

--- esbecommerce/view-produto.php
+++ esbecommerce/view-produto.php
@@ -15,6 +15,74 @@ include_once './conexao.php';
         <link rel="shortcut icon" href="imagens/icon/favicon.ico" >
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
         <title>Vizualizar Produto Disponivel</title>
+
+        <style>
+      /* estilo para a div */
+      .modal {
+        position: fixed;
+        top: 0;
+        right: 0;
+        width: 300px;
+        height: 100%;
+        background-color: #f2f2f2;
+        z-index: 1;
+        overflow-x: hidden;
+        transition: 0.5s;
+        padding-top: 60px;
+      }
+      
+      /* estilo para o botão de fechar o modal */
+      .close {
+        position: absolute;
+        top: 0;
+        right: 25px;
+        font-size: 35px;
+        margin-left: 50px;
+      }
+      
+      /* estilo para o conteúdo do modal */
+      .modal-content {
+        padding: 20px;
+      }
+      
+      /* estilo para a div escura que cobre a página quando o modal é aberto */
+      .modal-backdrop {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0,0,0,0.4);
+        z-index: 1;
+        display: none;
+      }
+      
+      /* estilo para a div que contém a página principal */
+      .main {
+        margin-right: 300px; /* largura da div do modal */
+      }
+      
+      /* estilo para o botão que abre o modal */
+      .open-modal {
+        background-color: #4CAF50; /* cor de fundo do botão */
+        color: white; /* cor do texto do botão */
+        padding: 16px 20px; /* espaçamento interno do botão */
+        border: none; /* borda do botão */
+        cursor: pointer; /* cursor ao passar o mouse sobre o botão */
+        font-size: 16px; /* tamanho do texto do botão */
+        position: fixed;
+        top: 50%;
+        right: 0;
+        transform: translate(0, -50%);
+        z-index: 1;
+      }
+      
+      /* estilo para o conteúdo da página principal */
+      .content {
+        padding: 20px;
+      }
+    </style>
+
     </head>
     <body>
 
@@ -45,12 +113,39 @@ include_once './conexao.php';
         <div class = "row">
         <div class ="col-md-12 mt-5">
         
-        <?php echo $descrição ; ?>
+
+        <div class="main">
+      <h1>Exemplo de div com modal à direita</h1>
+      <p>Este é um exemplo de como criar uma div no HTML5 para exibir um modal do lado direito da página.</p>
+      <button class="open-modal">Abrir modal</button>
+      <div class="content">
+        <!-- Conteúdo da página principal aqui -->
+      </div>
+    </div>
+
+    <!-- div que contém o modal -->
+    <div class="modal">
+      <span class="close">&times;</span>
+      <div class="modal-content">
+      <?php echo $descrição ; ?> <!-- Conteúdo do modal aqui -->
+      </div>
+    </div>
+    
+    <!-- div que cobre a página quando o modal é aberto -->
+    <div class="modal-backdrop"></div>
+
 
         </div>
         </div>
         </div>
         </div>
+
+        <script>
+      // código JavaScript para abrir e fechar o modal
+      const openModalButton = document.querySelector('.open-modal');
+      const modal = document.querySelector('.modal');
+      const modalBackdrop = document.querySelector('.modal-backdrop');
+      </script>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
