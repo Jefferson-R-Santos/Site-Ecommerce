d8cfe6a97f6c262151c6d6eda83e1ded56394bb7
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-08 13:48:24 -0300

    Update view-produto.php

--- esbecommerce/view-produto.php
+++ esbecommerce/view-produto.php
@@ -44,10 +44,10 @@ include_once './conexao.php';
         </div>
         <div class = "row">
         <div class ="col-md-12 mt-5">
-        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        
   <!-- Button trigger modal -->
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-  descrição
+  Descrição
 </button>
 <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog">
