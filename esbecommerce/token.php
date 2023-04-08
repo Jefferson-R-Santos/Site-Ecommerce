62c02478b85d7159c0c443de8daebf3314b0148e
Jefferson Ribeiro Santos <jeffersonribeiro2817@gmail.com>
2023-04-08 16:09:03 -0300

    Update token.php

--- esbecommerce/token.php
+++ esbecommerce/token.php
@@ -5,8 +5,8 @@ if (!defined('ACESSO')) {
     die("Erro: Pagina Não Encontrada");
 }
 
-define('PICPAYTOKEN', "seu token");
-define('XSELLERTOKEN', " seu x-seller-token");
+define('PICPAYTOKEN', "070e3567-ef60-440d-bef6-826a327e000e");
+define('XSELLERTOKEN', "95b8be0a-c56c-4961-906a-99694cd775f4");
 define('CALLBACKURL', "http://www.sualoja.com.br/callback");
 define('RETURNURL', "http://www.sualoja.com.br/cliente/pedido/");
 
